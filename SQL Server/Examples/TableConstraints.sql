c7f0d351085457a906c699da7e449e54d23bfd6e
Gavin Draper <gavdraper@gmail.com>
2018-11-21 08:05:40 +0000

    simplify

--- SQL Server/Examples/TableConstraints.sql
+++ SQL Server/Examples/TableConstraints.sql
@@ -2,7 +2,6 @@ CREATE TABLE named_constraints
 (
 	id int NOT NULL IDENTITY (1, 1),
 	added_by int NOT NULL,
-	person_id int NOT NULL,
 	[date] datetime NOT NULL CONSTRAINT df_table_default DEFAULT GETDATE(),
 	CONSTRAINT pk_table_id PRIMARY KEY CLUSTERED(Id),
 	CONSTRAINT fk_table_added_by FOREIGN KEY (added_by) REFERENCES [user](id),
