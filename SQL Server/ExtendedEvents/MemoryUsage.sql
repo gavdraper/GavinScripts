74b8a1dcf93b5c8fecd75a425b6f75a656b827fc
gavdraper <g@vindraper.co.uk>
2018-12-18 15:34:51 +0000

    comment

--- SQL Server/ExtendedEvents/MemoryUsage.sql
+++ SQL Server/ExtendedEvents/MemoryUsage.sql
@@ -1,8 +1,8 @@
-CREATE EVENT SESSION MemoryUseageAbove5Mb2 ON SERVER 
-ADD EVENT sqlserver.query_memory_grant_usage
+CREATE EVENT SESSION MemoryUseageAbove10mb ON SERVER 
+ADD EVENT sqlserver.query_memory_grant_usage --Ignores querys below 5mb 
 	(
         ACTION(sqlserver.sql_text)
-        WHERE ([granted_memory_kb] > 10000)
+        WHERE ([granted_memory_kb] > 10000) --Above 10mb
     )
 WITH 
 	(MAX_MEMORY=1024 KB)
