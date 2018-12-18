ba8cbdd832b60cff2e28d19ca186b10b2f97a439
gavdraper <g@vindraper.co.uk>
2018-12-18 15:33:03 +0000

    add predicate to filter out small memory grants

--- SQL Server/ExtendedEvents/MemoryUsage.sql
+++ SQL Server/ExtendedEvents/MemoryUsage.sql
@@ -1,5 +1,8 @@
-CREATE EVENT SESSION MemoryUseageAbove5Mb ON SERVER 
+CREATE EVENT SESSION MemoryUseageAbove5Mb2 ON SERVER 
 ADD EVENT sqlserver.query_memory_grant_usage
-	(ACTION(sqlserver.sql_text))
+	(
+        ACTION(sqlserver.sql_text)
+        WHERE ([granted_memory_kb] > 10000)
+    )
 WITH 
 	(MAX_MEMORY=1024 KB)
