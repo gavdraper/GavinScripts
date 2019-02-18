33343db60771193a359be28e662f93b9fa7cd840
Gavin Draper <g@vindraper.co.uk>
2019-02-18 11:15:34 +0000

    add buffercache script

---
+++ SQL Server/Maintenance/BufferCache.sql
@@ -0,0 +1,32 @@
+--Get total size of buffer cache
+SELECT (COUNT(*)*8)/1024 TotalSizeMb FROM sys.dm_os_buffer_descriptors
+
+--Get usage breakdown
+SELECT 
+    d.name [Database],
+    o.name [Table],
+    i.name [Index],
+    COUNT(*) AS [Pages],
+    (COUNT(*) *8)/1024 [SizeMB] ,
+    o.[type]
+FROM
+    sys.dm_os_buffer_descriptors bd
+    INNER JOIN sys.databases d ON d.database_id = bd.database_id
+    INNER JOIN sys.allocation_units au ON au.allocation_unit_id = bd.allocation_unit_id
+    INNER JOIN sys.partitions p ON  /* allocation_unit types : 0=Dropped, 1 = In Row, 2=LOB, 3=Row-Overflow data*/
+        (au.container_id = p.hobt_id AND au.[type] IN (1,3)) 
+        OR (au.container_id = p.partition_id AND au.[type] = 2)
+    INNER JOIN sys.objects o ON p.object_id = o.object_id
+    LEFT JOIN sys.indexes i ON i.object_id = o.object_id AND p.index_id = i.index_id
+ WHERE 
+    o.type <> 'S' --System base table
+    AND d.name NOT IN ('master','model','msdb')
+GROUP BY
+    d.name,
+    i.name,
+    o.name,    
+    au.type_desc,
+    o.type
+
+
+    
